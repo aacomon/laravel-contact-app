6057cc42ad64a7d097742d07eef616c530410bd3
aacomon <comon.anthony@gmail.com>
2023-05-27 03:18:12 +0800

    landing page

---
+++ resources/views/contacts/_contact.blade.php
@@ -0,0 +1,15 @@
+{{--<tr @if ($loop->odd) class="table-primary" @endif>  it will loop color (first, last, even and odd ) --}}
+<tr>
+    {{--<th scope="row">{{ $loop->index }}</th>--}}
+    <th scope="row">1</th>
+    <td>{{ $contact['name'] }}</td>
+    <td>{{ $contact['phone'] }}</td>
+    <td>alfred@test.com</td>
+    <td>Company one</td>
+    <td width="150">
+        {{-- <a href="{{ route('contacts.show', $id) }}" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a> --}}
+        <a href="{{ route('contacts.show', $contact['id']) }}" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
+        <a href="form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
+        <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
+    </td>
+</tr>
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -31,24 +31,15 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @if (count($contacts))
-                                    @foreach ($contacts as $id => $contact)
-                                        <tr>
-                                            <th scope="row">1</th>
-                                            <td>{{ $contact['name'] }}</td>
-                                            <td>{{ $contact['phone'] }}</td>
-                                            <td>alfred@test.com</td>
-                                            <td>Company one</td>
-                                            <td width="150">
-                                                <a href="{{ route('contacts.show', $id) }}" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
-                                                <a href="form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
-                                                <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @else
+                                {{-- @forelse ($contacts as $id => $contact)
+    
+                                    @include('contacts._contact', ['contact' => $contact])
+
+                                @empty
                                     <p>No contacts found</p>
-                                @endif
+                                @endforelse --}}
+
+                                @each('contacts._contact', $contacts, 'contact', 'contacts._empty')
                             </tbody>
                         </table>
 
