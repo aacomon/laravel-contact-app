0cfdf6a1579d99c549432b7c31a25d57d032e45b
aacomon <comon.anthony@gmail.com>
2023-06-02 03:11:51 +0800

    Update from section6 to section8-68

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,11 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\CompanyRepository;
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
-    public function index()
+    //protected $company;
+
+    public function __construct(protected CompanyRepository $company)
     {
+        //$this->company = $company;
     }
+
+
+
+    public function index(CompanyRepository $company)
+    {
+        // $companies = [
+        //     1 => ['name' => 'Company One', 'contacts' => 3],
+        //     2 => ['name' => 'Company Two', 'contacts' => 5],
+        // ];
+
+        $companies = $company->pluck();
+
+        $contacts = Contact::latest()->get();
+
+        //$contacts = $this->getContacts();  call the function getContacts above
+        // return view('contacts.index', ['contacts' => $contacts]);
+        return view('contacts.index', compact('contacts', 'companies'));
+    }
+
+    public function create()
+    {
+        return view('contacts.create');
+    }
+
+    public function show($id)
+    {
+        // $contacts = $this->getContacts();
+        $contact = Contact::findOrFail($id);
+        //abort_if(!isset($contacts[$id]), 404);  this will show when out of range and will show 404 pages instead of error
+        // abort_if(empty($contact), 404);
+        return view('contacts.show')->with('contact', $contact);
+    }
+
+    // protected function getContacts()
+    // {
+    //     return [
+    //         1 => ['id' => 1, 'name' => 'Name 1', 'phone' => '1234567890'],
+    //         2 => ['id' => 2, 'name' => 'Name 2', 'phone' => '2345678901'],
+    //         3 => ['id' => 3, 'name' => 'Name 3', 'phone' => '3456789012'],
+    //     ];
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ContactNoteController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,47 +20,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-function getContacts()
-{
-    return [
-        1 => ['id' => 1, 'name' => 'Name 1', 'phone' => '1234567890'],
-        2 => ['id' => 2, 'name' => 'Name 2', 'phone' => '2345678901'],
-        3 => ['id' => 3, 'name' => 'Name 3', 'phone' => '3456789012'],
-    ];
-}
+Route::get('/', WelcomeController::class);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(ContactController::class)->name('contacts.')->group(function () {
+
+    Route::get('/contacts', 'index')->name('index');
+
+    Route::get('/contacts/create', 'create')->name('create');
+
+    Route::get('/contacts/{id}', 'show')->name('show');
 });
 
-Route::get('/contacts', function () {
-    $companies = [
-        1 => ['name' => 'Company One', 'contacts' => 3],
-        2 => ['name' => 'Company Two', 'contacts' => 5],
-    ];
-    $contacts = getContacts(); // call the function getContacts above
-    // return view('contacts.index', ['contacts' => $contacts]);
-    return view('contacts.index', compact('contacts', 'companies'));
-})->name('contacts.index');
+Route::resource('/companies', CompanyController::class);
 
-Route::get('/contacts/create', function () {
-    return view('contacts.create');
-    //return "<h1>Add new Contact</h1>";
-})->name('contacts.create');
+Route::resources([
+    '/tags' => TagController::class,
+    '/tasks' => TaskController::class
+]);
 
-Route::get('/contacts/{id}', function ($id) {
-    $contacts = getContacts();
-    abort_if(!isset($contacts[$id]), 404); // this will show when out of range and will show 404 pages instead of error
-    $contact = $contacts[$id];
-    return view('contacts.show')->with('contact', $contact);
-})->name('contacts.show');
+// Route::resource('/activities', ActivityController::class)->only([
+//     'create', 'store', 'edit', 'update', 'destroy'
+// ]);
 
-Route::get('companies/{name?}', function ($name = null) {
-    if ($name) {
-        return "Company " . $name;
-    } else {
-        return "All companies";
-    }
-    //})->where('name', '[a-zA-Z]+'); // this will accept string or letters only
-    //})->whereAlpha('name'); //same will accept string or letters only
-})->whereAlphaNumeric('name'); // this will accept string or numbers
+// Route::resource('/activities', ActivityController::class)->except([
+//     'create', 'store', 'edit', 'update', 'destroy'
+// ]);
+
+Route::resource('/activities', ActivityController::class)->except([
+    'index', 'show'
+]);
+
+Route::resource('/contacts.notes', ContactNoteController::class)->shallow();
+
+// Route::resource('/activities', ActivityController::class)->names([
+//     'index' => 'activities.all',
+//     'show' => 'activities.view'
+// ]);
+
+Route::resource('/activities', ActivityController::class)->parameters([
+    'activities' => 'active'
+]);
